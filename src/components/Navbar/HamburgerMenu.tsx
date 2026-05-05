05faf13f524d3bf8a981cafcb3e15dbe083cf2ee
cristina <c1plamadeala@gmail.com>
2026-05-05 12:54:26 +0300

    Add Hamburger Menu and adjust css for mobile version

---
+++ src/components/Navbar/HamburgerMenu.tsx
@@ -0,0 +1,53 @@
+import styles from "./HamburgerMenu.module.css";
+
+interface Props {
+  isOpen: boolean;
+  toggle: () => void;
+}
+
+export function HamburgerMenu({ isOpen, toggle }: Props) {
+  return (
+    <button
+      className={`${styles.hamburgerButton} ${isOpen ? styles.active : ""}`}
+      onClick={toggle}
+      aria-label="Toggle menu"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+      >
+        <g clip-path="url(#clip0_356_20948)">
+          <path
+            d="M4 6H20"
+            stroke="#C026D3"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M4 12H20"
+            stroke="#C026D3"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M4 18H20"
+            stroke="#C026D3"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_356_20948">
+            <rect width="24" height="24" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    </button>
+  );
+}
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,28 @@
 import { Button } from "../Button/Button";
 import styles from "./Navbar.module.css";
 import type from "../../styles/typography.module.css";
+import { HamburgerMenu } from "./HamburgerMenu";
+// import { HamburgerMenu } from "./HamburgerMenu";
 
 export function Navbar() {
+  // Define state
+  // const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // const toggleMenu = () => {
+  //   setIsMenuOpen((prev) => !prev);
+  // };
   return (
     <nav className={styles.nav}>
       <div className={`${styles.logo} ${type.logo}`}>
         break<span className={styles.accent}>io</span>
       </div>
-
+      {/* Pass state and toggle function */}
+      {/* <HamburgerMenu isOpen={isMenuOpen} toggle={toggleMenu} /> */}
+      <HamburgerMenu
+        isOpen={false}
+        toggle={() => console.log("toggle clicked")}
+      />
+      {/* Apply conditional class for mobile visibility */}
       <div className={styles.links}>
         <a className={type.navItem} href="#explore">
           Explore
